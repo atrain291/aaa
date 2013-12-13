ffb771c7adcd22aa12082b76dafd3dbf6c029ba0
atrain291 <atrain291@gmail.com>
2013-12-13 08:52:59 -0500

    Second Commit to fix Project type in Test and Set TraderUI as the startup Project

--- src/app/Trader.UI/MainWindow.cs
+++ src/app/Trader.UI/MainWindow.cs
@@ -6,6 +6,7 @@ public partial class MainWindow: Gtk.Window
 	public MainWindow () : base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		//this.Show ();
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
